6d19b354680c90594bb02c50b1c9761de223b002
teste <arielalves.alves@gmail.com>
2026-08-19 21:19:03 -0400

    entendendo array

---
+++ Estrutura_e_Algoritimos/arrays.js
@@ -0,0 +1,18 @@
+// Demontrando array buffer em JS
+
+//[ 1111 0000 1111 0000 ]
+//[ 1111 0000 1111 0000 ]
+//[ 11 11 00 00 11 11 00 00 ]
+
+//
+// const a = new ArrayBuffer(8);
+// undefined
+//  a
+//  ArrayBuffer { [Uint8Contents]: <00 00 00 00 00 00 00 00>, [byteLength]: 8 }
+
+ // const a32 = new Uint32Array(a)
+//  undefined
+//  a32
+//  Uint32Array(2) [ 0, 0 ]
+
+
